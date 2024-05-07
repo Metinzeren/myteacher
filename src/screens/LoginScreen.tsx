34150dc2a039a39614c11568b9f323e394b298ea
metinzeren <zeren0760@hotmail.com>
2024-05-07 17:39:39 +0300

    login screen push

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity ,Image} from 'react-native';
 import styled from 'styled-components/native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
@@ -11,28 +11,37 @@ export default function LoginScreen() {
 
   return (
     <Container>
-      <Form>
-        <LoginTitle>MY TEACHER</LoginTitle>
-        <InputContainer>
-          <InputIcon icon={faUser} />
-          <Input
-            placeholder="Eposta adresinizi girin"
-            placeholderTextColor={colors.lightGray}
+      <LoginScreenSection>
+        <LoginTopSection>
+          <Image
+            source={{ uri:'https://images.vexels.com/media/users/3/128199/isolated/preview/b354bc4707224bd3d15b9ae36eca70c0-male-student-cartoon.png?width=320' }}
+            style={{ width: 260, height: 260}}
+            resizeMode="contain"
           />
-        </InputContainer>
-        <InputContainer>
-          <InputIcon icon={faLock} />
-          <Input
-            placeholder="Şifrenizi girin"
-            placeholderTextColor={colors.lightGray}
-            secureTextEntry={true}
-          />
-        </InputContainer>
-        <RegisterTitle>Hesabın yok mu? <RegisterTitleLink onPress={() => console.log("dfhsdfh")}>Kayıt ol</RegisterTitleLink> </RegisterTitle>
-        <LoginButton onPress={() => console.log("Giriş yapıldı")}>
-          <LoginButtonText>GİRİŞ</LoginButtonText>
-        </LoginButton>
-      </Form>
+        </LoginTopSection>
+        <LoginBottomSection>
+          <InputContainer>
+            <InputIcon icon={faEnvelope} />
+            <Input
+              placeholder="Email"
+              placeholderTextColor={"grey"}
+            />
+          </InputContainer>
+          <InputContainer>
+            <InputIcon icon={faLock} />
+            <Input
+              placeholder="Şifre"
+              placeholderTextColor={"grey"}
+            />
+          </InputContainer>
+          <TouchableOpacity onPress={() => console.log("Şifremi Unuttum")}>
+            <ForgotPasswordText>Şifremi Unuttum</ForgotPasswordText>
+          </TouchableOpacity>
+          <LoginButton onPress={() => console.log("Giriş yapıldı")}>
+            <LoginButtonText>Giriş Yap</LoginButtonText>
+          </LoginButton>
+        </LoginBottomSection>
+      </LoginScreenSection>
     </Container>
   );
 }
@@ -43,61 +52,67 @@ const Container = styled(View)`
   background-color: #ffffff;
 `;
 
-const Form = styled(View)`
-  width: 80%;
-  justify-content: center;
-  align-items: center;
-  margin-top: 50%;
-`
+const LoginScreenSection = styled(View)`
+  height: 100%;
+  width: 100%;
+  flex: 1;
+  background-color: #d3e0c4;
+`;
 
-const LoginTitle = styled(Text)`
-  color: black;
-  font-size: 25px;
-  margin-bottom: 50px;
-  fontWeight:bold;
+const LoginTopSection = styled(View)`
+  align-items:center;
+  justify-content:center;
+  width:100%;
+  height:45%;
 `
+const LoginTopSectionImage = styled(Text)`
+`
+const LoginBottomSection = styled(View)`
+  width: 100%;
+  height: 55%;
+  background-color: white;
+  border-radius: 50px 50px 0 0;
+  align-items:center;
+  padding-top:50px;
+`;
 
 const InputContainer = styled(View)`
-  width: 100%;
+  width: 85%;
   flex-direction: row;
   align-items: center;
   margin-bottom: 15px;
-  position: relative;
 `
 
 const InputIcon = styled(FontAwesomeIcon)`
-  color: black;
+  color: grey;
   font-size: 20px;
-  position: absolute;
 `
-
 const Input = styled(TextInput)`
   flex: 1;
   height: 40px;
-  padding: 8px 8px 8px 20px;
+  padding: 8px 8px 8px 5px;
+  font-weight:bold;
   border-bottom-width: 1px;
   border-bottom-color: #cccccc;
-`
+`;
+
 
 const LoginButton = styled(TouchableOpacity)`
-  width: 50%;
-  height: 40px;
-  background-color: black;
+  background-color: #b44a4a;
+  border-radius: 10px;
+  padding: 12px 50px 12px 50px;
   align-items: center;
   justify-content: center;
-  margin-top: 10px;
+  margin-top: 25px;
 `;
 
 const LoginButtonText = styled(Text)`
   color: #ffffff;
-  font-size: 17px;
+  font-size: 16px;
 `;
-
-const RegisterTitle = styled(Text)`
-  color:black;
-  font-size:15px;
-`
-const RegisterTitleLink = styled(Text)`
-  color:orange;
+const ForgotPasswordText = styled(Text)`
+  color: #b44a4a;
+  font-size: 14px;
+  margin-top: 15px;
   font-weight:bold;
-`
+`;

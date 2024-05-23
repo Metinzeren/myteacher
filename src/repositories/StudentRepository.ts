29fa64122be607238fc93866c50ac46d169e121c
Özkan Kocakaplan <ozkankocakaplan07@gmail.com>
2024-05-23 23:55:18 +0300

    add updateStudent, deleteStudent, getStudent metod

--- src/repositories/StudentRepository.ts
+++ src/repositories/StudentRepository.ts
@@ -1,22 +1,47 @@
-import { collection, doc, setDoc } from "firebase/firestore";
-import { db } from "../firebase/config";
-import FirebaseCollections from "../firebase/FirebaseCollections";
-import Student from "../models/Student";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from 'firebase/firestore';
+import {db} from '../firebase/config';
+import FirebaseCollections from '../firebase/FirebaseCollections';
+import Student from '../models/Student';
 
 class StudentRepository {
-    private static instance: StudentRepository;
-    private studentCollection = collection(db, FirebaseCollections.STUDENTS);
-    public static getInstance(): StudentRepository {
-        if (!StudentRepository.instance) {
-            StudentRepository.instance = new StudentRepository();
-        }
-        return StudentRepository.instance;
+  private static instance: StudentRepository;
+  private studentCollection = collection(db, FirebaseCollections.STUDENTS);
+  public static getInstance(): StudentRepository {
+    if (!StudentRepository.instance) {
+      StudentRepository.instance = new StudentRepository();
     }
-    private constructor() { }
-    async addStudent(student: Student) {
-        const studentDoc = doc(this.studentCollection);
-        student.id = studentDoc.id;
-        await setDoc(studentDoc, student);
-    }
+    return StudentRepository.instance;
+  }
+  private constructor() {}
+
+  async addStudent(student: Student) {
+    const studentDoc = doc(this.studentCollection);
+    student.id = studentDoc.id;
+    await setDoc(studentDoc, student);
+  }
+  async updateStudent(student: Student) {
+    const studentDoc = doc(this.studentCollection, student.id);
+    await setDoc(studentDoc, student);
+  }
+  async deleteStudent(studentId: string) {
+    const studentDoc = doc(this.studentCollection, studentId);
+    await deleteDoc(studentDoc);
+  }
+  async getStudent(studentId: string) {
+    const studentDoc = doc(this.studentCollection, studentId);
+    const studentSnapshot = await getDoc(studentDoc);
+    return studentSnapshot.data() as Student;
+  }
+  async getAllStudents() {
+    const studentSnapshot = await getDocs(this.studentCollection);
+    return studentSnapshot.docs.map(doc => doc.data() as Student);
+  }
 }
 export default StudentRepository;

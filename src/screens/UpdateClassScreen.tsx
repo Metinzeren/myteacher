971045f22d6642a40901be4795749490b1dbe06f
metinzeren <zeren0760@hotmail.com>
2024-06-19 16:08:37 +0300

    updated added

--- src/screens/UpdateClassScreen.tsx
+++ src/screens/UpdateClassScreen.tsx
@@ -1,17 +1,17 @@
-import {View, Text} from 'react-native';
-import React, {useRef, useState} from 'react';
-import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
-import {RootStackParamList} from '../types/Navigation';
+import { View, Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types';
+import { RootStackParamList } from '../types/Navigation';
 import Container from '../components/Container/Container';
-import FormContainer, {FormContainerRef} from '../components/FormContainer';
+import FormContainer, { FormContainerRef } from '../components/FormContainer';
 import Input from '../components/Input/Input';
-import {faUser} from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import ClassRoom from '../models/ClassRoom';
 import Button from '../components/Button/Button';
-import {t} from 'i18next';
+import { t } from 'i18next';
 import AlertDialog from '../components/AlertDialog/AlertDialog';
 import ClassRoomRepository from '../repositories/ClassRoomRepository';
-import {useClassRooms} from '../context/ClassRoomContext';
+import { useClassRooms } from '../context/ClassRoomContext';
 
 export default function UpdateClassScreen(
   props: NativeStackScreenProps<RootStackParamList, 'UpdateClassScreen'>,
@@ -21,7 +21,7 @@ export default function UpdateClassScreen(
   const classRoomRepo = ClassRoomRepository.getInstance();
   const [className, setClassName] = useState(classInfo);
 
-  const {updateClassRoom} = useClassRooms();
+  const { updateClassRoom } = useClassRooms();
 
   const handleChange = (key: keyof ClassRoom, value: string) => {
     setClassName(prevState => ({
@@ -30,34 +30,38 @@ export default function UpdateClassScreen(
     }));
   };
   const updateClass = () => {
-    classRoomRepo
-      .updateClassRoom(className)
-      .then(res => {
-        if (res) {
-          updateClassRoom(res);
+    console.log(className, "classname logladım");
+    const result = formRef.current?.validate({ className: "Bu alanı boş bırakamazsınız." })
+    if (result) {
+      classRoomRepo
+        .updateClassRoom(className)
+        .then(res => {
+          if (res) {
+            updateClassRoom(res);
+            AlertDialog.showModal({
+              title: 'Başarılı',
+              message: 'Sınıf başarıyla güncellendi',
+              onConfirmText: 'Tamam',
+              onConfirm: () => {
+                props.navigation.goBack();
+              },
+            });
+          }
+        })
+        .catch(er => {
+          console.log(er);
           AlertDialog.showModal({
-            title: 'Başarılı',
-            message: 'Sınıf başarıyla güncellendi',
-            onConfirmText: 'Tamam',
-            onConfirm: () => {
-              props.navigation.goBack();
-            },
+            title: 'Hata',
+            message: 'Sınıf güncellenirken bir hata oluştu',
           });
-        }
-      })
-      .catch(er => {
-        console.log(er);
-        AlertDialog.showModal({
-          title: 'Hata',
-          message: 'Sınıf güncellenirken bir hata oluştu',
         });
-      });
+    }
+
   };
   return (
     <Container p={10} header title="Sınıf Güncelle" goBackShow>
       <FormContainer
-        style={{gap: 10}}
-        formId="addStudentForm"
+        style={{ gap: 10 }}
         formContainerRef={formRef}>
         <Input
           required
